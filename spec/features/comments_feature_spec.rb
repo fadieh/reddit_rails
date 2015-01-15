9627b98f16ea095c71c3e9f7b090c6d4976d21e7
Fadie Hannona <fadiehannona@gmail.com>
2015-01-15 21:59:46 +0000

    allow user to leave comments on posts

--- spec/features/comments_feature_spec.rb
+++ spec/features/comments_feature_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
-feature 'reviewing' do
-	before {Post.create name: 'Google', url: 'http://www.google.com'}
+feature 'commenting' do
+	let!(:google){ Post.create(title: 'Google', url: 'http://www.google.com')}
 
-	xscenario 'allows user to leave a comment using a form' do
-		visit '/posts'
-		click_link 'Comment'
+	scenario 'allows user to leave a comment using a form' do
+		visit "/posts"
+		click_link "Comment"
 		fill_in "Thoughts", with: "So so"
 		click_button 'Save'
 
-		expect(current_path).to eq '/posts'
-		expect(page).to have_content('so so')
+		expect(current_path).to eq "/posts"
+		expect(page).to have_content('So so')
 	end
 
 end
